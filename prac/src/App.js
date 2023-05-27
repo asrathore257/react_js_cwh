384ad0792b47eb42d04c7a90a102969785009ac5
aditya <asrathore257@gmail.com>
2023-05-27 13:25:25 +0530

    video 5

---
+++ prac/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="blank">Lovely</div>
+  );
+}
+//in jsx camel case for htmlFor,tableIndex and if functional component then use <> </> after return type and
+//if we have to write javascript then use {}curly braces 
+//babel compile it  
+export default App;
